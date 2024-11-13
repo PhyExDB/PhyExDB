6f48f1238a2b140ceecb054c97d2ef5d6e4de630
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2024-11-13 14:38:33 +0100

    first RestController

--- backend/src/main/kotlin/de/phyexdb/phyexdb/PhyexdbApplication.kt
+++ backend/src/main/kotlin/de/phyexdb/phyexdb/PhyexdbApplication.kt
@@ -2,10 +2,20 @@ package de.phyexdb.phyexdb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class PhyexdbApplication
 
 fun main(args: Array<String>) {
 	runApplication<PhyexdbApplication>(*args)
+}
+
+@RestController
+class HelloWorldController {
+	@GetMapping("/hello")
+	fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
