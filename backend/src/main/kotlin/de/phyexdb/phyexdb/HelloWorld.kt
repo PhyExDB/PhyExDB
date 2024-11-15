40e898dc0f2bfcf9954d74eded6f3bee8fb9ef3c
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2024-11-15 12:25:26 +0100

    added comments

--- backend/src/main/kotlin/de/phyexdb/phyexdb/HelloWorld.kt
+++ backend/src/main/kotlin/de/phyexdb/phyexdb/HelloWorld.kt
@@ -4,8 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+/** Hello World Controller */
 @RestController
 class HelloWorldController {
+	/** Hello World Mapping */
 	@GetMapping("/hello") //http://localhost:8080/hello?name=World
 	fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
