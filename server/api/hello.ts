bc1cea5e7f6ae5650b2836fb7b52c771379fb221
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-17 19:46:48 +0100

    Add hello world endpoint

---
+++ server/api/hello.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+
+  if (query.name) {
+    return `Hello ${query.name}`
+  } else {
+    return 'Hello World'
+  }
+})
