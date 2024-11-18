cfe0e66836434495b164b45e4bbb99487809763a
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-19 00:05:59 +0100

    Add Docker and eslint and configure GitHub actions (#22)
    * Add docker
    
    * Add eslint
    
    * Run eslint
    
    * Add GitHub Actions
    
    * Add package permissions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  modules: ["@nuxt/eslint"],
 
   devtools: { enabled: true },
 
   future: {
     compatibilityVersion: 4,
+  },
+
+  compatibilityDate: "2024-04-03",
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: "always-multiline",
+        braceStyle: "1tbs",
+        indent: 2,
+        quotes: "double",
+        semi: false,
+      },
+    },
   },
 })
