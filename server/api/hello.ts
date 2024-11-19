ac72e70b40249a400330c4a96a308d52466ba7b6
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-19 14:42:47 +0100

    Check db connection

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,5 +1,14 @@
+import sequelize from "../database/config"
+
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
+
+  try {
+    await sequelize.authenticate()
+    console.log("Connection has been established successfully.")
+  } catch (error) {
+    console.error("Unable to connect to the database:", error)
+  }
 
   if (query.name) {
     return `Hello ${query.name}`
