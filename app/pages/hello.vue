1278ae1c660817dc0bb46dd2aa15116af2b41254
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-20 21:43:11 +0100

    Add basic page which uses api to call backend

---
+++ app/pages/hello.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const { data: hello } = await useAPI<string>("/hello")
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <p>{{ hello }}</p>
+  </div>
+</template>
