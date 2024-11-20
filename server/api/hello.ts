194d5191afe7dd8993f6753baeb4a939a99a9d34
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-20 14:29:16 +0100

    Remove console.log statements

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,17 +1,8 @@
-import sequelize from "../database/config"
 import User from "../models/User"
 
 export default defineEventHandler(async () => {
-  try {
-    await sequelize.authenticate()
-    console.log("Connection has been established successfully.")
-  } catch (error) {
-    console.error("Unable to connect to the database:", error)
-  }
-
   const users = await User.findAll()
   let description: string = ""
-  console.log(users.length)
   for (let i = 0; i < users.length; i++) {
     description += `[Name = ${users[i].getDataValue("username")}, Mail = ${users[i].getDataValue("email")}, Password = ${users[i].getDataValue("password")}] \t`
   }
