92dcfcf5ee9473ae5cc01e12485a16940b37146e
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-22 01:34:10 +0100

    Fix docker container (#29)
    * Add missing ports to docker compose
    
    * Require healthy db before backend start
    
    * Add some type packages
    
    * Restructure db to be a plugin
    
    * Bundle pg
    
    * Update packages and move some to dev dependencies
    
    * Don't alter table in production
    
    * Use dotenv to start dev with development env
    
    * Remove logs
    
    (cherry picked from commit d4db18f0b3173e58d57eba61d73c628f9c660b31)

---
+++ server/plugins/db-init.ts
@@ -0,0 +1,18 @@
+import sequelize from "../utils/sequelize"
+import "pg" // Do not remove this, because otherwise pg will not be included in the final bundle
+
+export default defineNitroPlugin(async () => {
+  try {
+    // Connect to the database
+    await sequelize.authenticate()
+
+    // Sync all models
+    if (process.env.NODE_ENV === "development") {
+      await sequelize.sync({ alter: true })
+    } else {
+      await sequelize.sync()
+    }
+  } catch (error) {
+    console.error("Unable to connect to the database:", error)
+  }
+})
