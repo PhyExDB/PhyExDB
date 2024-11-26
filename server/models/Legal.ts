215d651354c806891da3944f060fe575ff326a72
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-26 15:31:44 +0100

    Add legal model

---
+++ server/models/Legal.ts
@@ -0,0 +1,31 @@
+import { DataTypes, Model } from "sequelize"
+
+class Legal extends Model { }
+
+Legal.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "legal",
+  },
+)
+
+export default Legal
