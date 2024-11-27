422b858a898968e4688c59e4601fa5480c2922c5
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-27 19:50:26 +0100

    Use logger instead of console.log

--- server/plugins/db-init.ts
+++ server/plugins/db-init.ts
@@ -4,9 +4,9 @@ import { umzug, seed } from "../utils/umzug"
 export default defineNitroPlugin(async () => {
   try {
     const migrations = await umzug.up()
-    console.log("Migrations executed:", migrations)
+    logger.info("Migrations executed:", migrations)
     const seeds = await seed.up()
-    console.log("Seeds executed:", seeds)
+    logger.info("Seeds executed:", seeds)
   } catch (error) {
     console.error("Unable to connect to the database:", error)
   }
