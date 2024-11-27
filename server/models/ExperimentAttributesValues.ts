9fa9feb3fe66cc84bbc8e52cc3e26b6723462e69
FinnKeller <149683868+FinnKeller@users.noreply.github.com>
2024-11-27 17:31:47 +0100

    Add attribute Models (#52)
    * das ist ein Test
    
    * add Model for experiment attributes and for values of experiment attributes
    
    * removed reference in name
    
    * removed references

---
+++ server/models/ExperimentAttributesValues.ts
@@ -0,0 +1,26 @@
+import { DataTypes, Model } from "sequelize"
+import sequelize from "../utils/sequelize"
+
+class ExperimentAttributesValues extends Model {}
+
+ExperimentAttributesValues.init(
+  {
+    // Model attributes are defined here
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    // Other model options go here
+    sequelize, // connection instance
+    modelName: "ExperimentAttributesValues", // model name
+  },
+)
+
+export default ExperimentAttributesValues
