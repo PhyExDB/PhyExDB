57b2980c4e7b7c11e201dab15b2395747c9556e5
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-30 21:13:08 +0100

    Add tests

---
+++ tests/integration/server/api/legal/[slug].put.test.ts
@@ -0,0 +1,95 @@
+import { describe, expect, it } from "vitest"
+import { setup, $fetch, url } from "@nuxt/test-utils/e2e"
+import { v4 as uuidv4 } from "uuid"
+import Legal from "~~/server/database/models/Legal"
+
+describe("Api Route PUT /api/legal/{slug}", async () => {
+  await setup()
+
+  it.each([
+    { slug: "privacy-policy" },
+    { slug: "terms-of-service" },
+    { slug: "imprint" },
+  ])("should return the updated detail of the legal document with slug $slug", async ({ slug }) => {
+    const updateContent = {
+      name: `New name ${uuidv4()}`,
+      content: `New content ${uuidv4()}`,
+    }
+
+    const response = await $fetch(`/api/legal/${slug}`, {
+      method: "PUT",
+      body: JSON.stringify(updateContent),
+    })
+
+    // Expect the slug to be present
+    expect(response).toHaveProperty("slug")
+    expect(response.slug).toBe(slug)
+
+    // Expect the respective name
+    expect(response).toHaveProperty("name")
+    expect(response.name).toBe(updateContent.name)
+
+    // Expect the new content
+    expect(response).toHaveProperty("content")
+    expect(response.content).toBe(updateContent.content)
+
+    // Expect a UUID for each field
+    expect(response).toHaveProperty("id")
+    expect(response.id).toMatch(
+      /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+    )
+  })
+
+  it.each([
+    { slug: "privacy-policy" },
+    { slug: "terms-of-service" },
+    { slug: "imprint" },
+  ])("should return the updated detail of the legal document with slug $slug when getting by id", async ({ slug }) => {
+    const updateContent = {
+      name: `New name ${uuidv4()}`,
+      content: `New content ${uuidv4()}`,
+    }
+
+    const id = (await Legal.findOne({ where: { slug: slug } }))!.id
+    const response = await $fetch(`/api/legal/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(updateContent),
+    })
+
+    // Expect the slug to be present
+    expect(response).toHaveProperty("slug")
+    expect(response.slug).toBe(slug)
+
+    // Expect the respective name
+    expect(response).toHaveProperty("name")
+    expect(response.name).toBe(updateContent.name)
+
+    // Expect the new content
+    expect(response).toHaveProperty("content")
+    expect(response.content).toBe(updateContent.content)
+
+    // Expect a UUID for each field
+    expect(response).toHaveProperty("id")
+    expect(response.id).toBe(id)
+  })
+
+  it.each(["name", "content"])("should return an error when the field %s is empty", async (field) => {
+    const response = await fetch(url("/api/legal/imprint"), {
+      method: "PUT",
+      body: JSON.stringify({
+        name: field === "name" ? "" : `New name ${uuidv4()}`,
+        content: field === "content" ? "" : `New content ${uuidv4()}`,
+      }),
+    })
+
+    expect(response.status).toBe(400)
+  })
+
+  it("should return an error when an unknown slug is passed", async () => {
+    const response = await fetch(url("/api/legal/somethingwrong"), {
+      method: "PUT",
+    })
+
+    expect(response.status).toBe(404)
+  })
+})
