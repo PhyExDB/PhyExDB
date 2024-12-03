299536fdf2be79e8ee28d6fbc4a14c82b06ba3b4
niklhut <49069026+niklhut@users.noreply.github.com>
2024-12-03 12:09:27 +0100

    Fix imports (#74)
    * Fix docker compose
    
    * Fix imports

--- server/plugins/db-init.ts
+++ server/plugins/db-init.ts
@@ -1,4 +1,6 @@
 import "pg" // Do not remove this, because otherwise pg will not be included in the final bundle
+import sequelize from "~~/server/utils/sequelize"
+import logger from "~~/server/utils/loggers"
 
 export default defineNitroPlugin(async () => {
   try {
