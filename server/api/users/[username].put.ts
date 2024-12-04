4f63591280a81c413a0c4b3737edb2f3bdf835e0
LeviAchenbach <149683881+LeviAchenbach@users.noreply.github.com>
2024-12-04 13:38:18 +0100

    Added API for updateing a user and corrected API documentation in some files

--- server/api/users/[username].put.ts
+++ server/api/users/[username].put.ts
@@ -1,27 +1,42 @@
 import * as v from "valibot"
 import { readValidatedBody } from "h3"
+import { validate as uuidValidate } from "uuid"
 import Users from "~~/server/database/models/User"
 
 export default defineEventHandler(async (event) => {
+  // Extract username or id from the event
+  const usernameOrId = getRouterParam(event, "username")
+  if (!usernameOrId) {
+    throw createError({ status: 400, message: "Invalid username or id" })
+  }
+
+  // Find user to update
+  const isId = uuidValidate(usernameOrId)
+  const whereClause = isId ? { id: usernameOrId } : { username: usernameOrId }
+  const user = await Users.findOne({
+    where: whereClause,
+  })
+
+  // Check that user exists
+  if (!user) {
+    throw createError({ status: 404, message: "User not found" })
+  }
+
   // Validate user data
   const userSchema = v.object({
     username: v.pipe(v.string(), v.nonEmpty("Please enter Name")),
     email: v.pipe(v.string(), v.nonEmpty("Please enter Email"), v.email("Not an Email")),
-    password: v.pipe(v.string(), v.nonEmpty("Please enter Password"), v.minLength(8, "Password must be at least 8 characters"), v.regex(/[a-z]/, "Password must contain at least one lowercase letter"), v.regex(/[A-Z]/, "Password must contain at least one uppercase letter"), v.regex(/[0-9]/, "Password must contain at least one number")),
   })
 
   // This is a helper function that reads the body and validates it against the schema
-  const newUserContent = await readValidatedBody(event, body => v.parse(userSchema, body))
+  const updateUserContent = await readValidatedBody(event, body => v.parse(userSchema, body))
 
-  // Create new user
-  const user = await Users.create({ username: newUserContent.username, email: newUserContent.email, passwordHash: newUserContent.password, verified: false, role: "User" })
-
-  return user.toUserDetail()
+  return (await user.update(updateUserContent)).toUserDetail()
 })
 
 defineRouteMeta({
   openAPI: {
-    description: "Creates a new user",
+    description: "Updates a user",
     tags: ["User"],
     requestBody: {
       description: "User data",
@@ -33,16 +48,15 @@ defineRouteMeta({
             properties: {
               username: { type: "string" },
               email: { type: "string" },
-              password: { type: "string" },
             },
-            required: ["username", "email", "password"],
+            required: ["username", "email"],
           },
         },
       },
     },
     responses: {
       200: {
-        description: "User created successfully",
+        description: "User updated sucessfully",
         content: {
           "application/json": {
             schema: {
