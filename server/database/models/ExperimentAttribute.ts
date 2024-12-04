2f86d14051b515ed7d0f3ac93804da117b48e7e7
Keller, Finn <finn.keller@stud.tu-darmstadt.de>
2024-12-04 11:07:14 +0100

    getAttribute Endpoint to get the id and name of the Attribute. Created new shared type therefore and changed the model

--- server/database/models/ExperimentAttribute.ts
+++ server/database/models/ExperimentAttribute.ts
@@ -1,7 +1,22 @@
 import { DataTypes, Model } from "sequelize"
+import sequelize from "~~/server/utils/sequelize"
+import type { AttributeList } from "~~/shared/types/experiment/ExperimentAttribute.type"
 
-class ExperimentAttribute extends Model {}
-
+class ExperimentAttribute extends Model {
+  declare id: string
+  declare name: string
+  /**
+   * Converts the current instance to a LegalList object.
+   *
+   * @returns {AttributeList} An object containing the id, name, and slug of the current instance.
+   */
+  toAttributeList(): AttributeList {
+    return {
+      id: this.id,
+      name: this.name,
+    }
+  }
+}
 ExperimentAttribute.init(
   {
     // Model attributes are defined here
