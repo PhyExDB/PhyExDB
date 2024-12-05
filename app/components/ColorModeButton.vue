03469e600c8d0b54c3e93d84fa9fe87fc9543909
niklhut <49069026+niklhut@users.noreply.github.com>
2024-12-05 20:33:20 +0100

    Fix color mode button

--- app/components/ColorModeButton.vue
+++ app/components/ColorModeButton.vue
@@ -14,14 +14,13 @@ const colorMode = useColorMode()
       >
         <Icon
           name="heroicons:moon"
-          class="w-4 h-4"
-        >
-          <Icon
-            name="heroicons:sun"
-            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-          />
-          <span class="sr-only">Toggle theme</span>
-        </icon>
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <Icon
+          name="heroicons:sun"
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+        <span class="sr-only">Toggle theme</span>
       </Button>
     </DropdownMenuTrigger>
     <DropdownMenuContent align="end">
