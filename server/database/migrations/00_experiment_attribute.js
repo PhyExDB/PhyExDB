c3032b0dfa59477ad2506d7b4cd20e04ae152be9
FinnKeller <149683868+FinnKeller@users.noreply.github.com>
2024-12-06 17:47:39 +0100

    Add Migrations and Seeds for ExperimentAttribute and ExperimentValue (#94)
    * getAttribute Endpoint to get the id and name of the Attribute. Created new shared type therefore and changed the model
    
    * new version of get Attribute Endpoint
    
    * added openAPI documentation for eventhandler and documentation
    
    * added Endpoint for getting Values per Attribute and Updating the name of the Attribute
    
    * added Endpoint to update Value
    
    * added Endpoint to add value to an existing Attribute
    
    * aaded comments
    
    * fixed small mistakes not sure about linkage between attribute and value as the function toAttributeDetailWithAdditionalValue is doing that already
    
    * added id of attribute to created Value
    
    * added migrations of Value and Attribute of Experiment
    
    * seed for attributes
    
    * added Seeds for Attribute and Value
    
    * small fixes
    
    * removed import, table as a string
    
    * a
    
    * v
    
    * z

---
+++ server/database/migrations/00_experiment_attribute.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize"
+/**
+ * run the Attribute
+ */
+async function up({ context: queryInterface }) {
+  await queryInterface.createTable("ExperimentAttributes", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  })
+}
+/**
+ * revert the Experiment Attribute Migration
+ */
+async function down({ context: queryInterface }) {
+  await queryInterface.dropTable("ExperimentAttributes")
+}
+
+export { up, down }
