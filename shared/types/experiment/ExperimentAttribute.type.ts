e45c56334b89265b1d3755b417408acd18532354
Keller, Finn <finn.keller@stud.tu-darmstadt.de>
2024-12-06 11:57:09 +0100

    aaded comments

--- shared/types/experiment/ExperimentAttribute.type.ts
+++ shared/types/experiment/ExperimentAttribute.type.ts
@@ -1,7 +1,5 @@
 import type { BaseList } from "../Base.type"
-/**
-* represents an AttributeList with id and name
-*/
+/** represents an AttributeList with id and name */
 export interface AttributeList extends BaseList {
   /**
   * the id of the Attribute
@@ -12,10 +10,25 @@ export interface AttributeList extends BaseList {
    */
   name: string
 }
+/**
+ * represents an AttributeValue List with id and name
+ */
 export interface AttributeValue extends BaseList {
+  /**
+   * the id of the value
+   */
   id: string
+  /**
+   * the value of the attribute
+   */
   name: string
 }
+/**
+ * extends AttributeList and adds AttributeValue
+ */
 export interface AttributeDetail extends AttributeList {
+  /**
+   * attribute Value as an Array
+   */
   valueList: AttributeValue[]
 }
