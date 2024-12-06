c9fc04b91b11cfefb1b768dcaf57996593130298
Keller, Finn <finn.keller@stud.tu-darmstadt.de>
2024-12-06 14:29:06 +0100

    added migrations of Value and Attribute of Experiment

---
+++ server/database/migrations/experiment/00_Attribute.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize"
+/**
+ * run the Attribute
+ */
+async function up({ context: queryInterface }) {
+  await queryInterface.createTable("Attribute", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.Date,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.Date,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  })
+}
+/**
+ * revert the Experiment Attribute Migration
+ */
+async function down({ context: queryInterface }) {
+  await queryInterface.dropTable("Attribute")
+}
+
+export { up, down }
---
+++ server/database/migrations/experiment/00_Value.js
@@ -0,0 +1,43 @@
+import { DataTypes } from "sequelize"
+import ExperimentAttribute from "../../models/ExperimentAttribute"
+/**
+ * run the value migration
+ */
+async function up({ context: queryInterface }) {
+  await queryInterface.createTable("Value", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    attributeValue: {
+      type: DataTypes.UUIDV4,
+      refereces: {
+        model: ExperimentAttribute,
+        key: "id",
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.Date,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.Date,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  })
+}
+/**
+ * revert the Experiment Value Migration
+ */
+async function down({ context: queryInterface }) {
+  await queryInterface.dropTable("Value")
+}
+
+export { up, down }
