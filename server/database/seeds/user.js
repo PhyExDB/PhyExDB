07824bf56cdf4555ad2132cde34880241cf56317
niklhut <49069026+niklhut@users.noreply.github.com>
2024-12-06 11:33:16 +0100

    Revert "Fix user seeds"
    This reverts commit a35c89412d0286b365f174ab8026e251c9a9c995.

--- server/database/seeds/user.js
+++ server/database/seeds/user.js
@@ -1,15 +1,13 @@
 import { v4 as uuidv4 } from "uuid"
-import bcrypt from "bcrypt"
 
 /**
  * Seed the User table with initial data.
  */
 export const up = async ({ context: queryInterface }) => {
-  const salt = bcrypt.genSaltSync()
   await queryInterface.bulkInsert("Users", [
-    { id: uuidv4(), username: "Alice", role: "Administrator", email: "Alice@gmail.com", passwordHash: bcrypt.hashSync("secure", salt), verified: true, createdAt: new Date(), updatedAt: new Date() },
-    { id: uuidv4(), username: "Bob", role: "Moderator", email: "Bob@gmail.com", passwordHash: bcrypt.hashSync("ILikeAlice", salt), verified: true, createdAt: new Date(), updatedAt: new Date() },
-    { id: uuidv4(), username: "Eve", role: "User", email: "Eve@gmail.com", passwordHash: bcrypt.hashSync("password", salt), verified: true, createdAt: new Date(), updatedAt: new Date() },
+    { id: uuidv4(), username: "Alice", role: "Administrator", email: "Alice@gmail.com", passwordHash: "secure", verified: true, createdAt: new Date(), updatedAt: new Date() },
+    { id: uuidv4(), username: "Bob", role: "Moderator", email: "Bob@gmail.com", passwordHash: "ILikeAlice", verified: true, createdAt: new Date(), updatedAt: new Date() },
+    { id: uuidv4(), username: "Eve", role: "User", email: "Eve@gmail.com", passwordHash: "password", verified: true, createdAt: new Date(), updatedAt: new Date() },
   ])
 }
 
