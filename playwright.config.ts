e6fd4ec71a7635fbbe0a098884e773ac0bb91b65
niklhut <49069026+niklhut@users.noreply.github.com>
2024-12-06 21:05:48 +0100

    Improve playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,9 @@
 import { fileURLToPath } from "node:url"
-import { defineConfig } from "@playwright/test"
+import { defineConfig, devices } from "@playwright/test"
 import type { ConfigOptions } from "@nuxt/test-utils/playwright"
+import dotenv from "dotenv"
+
+dotenv.config({ path: ".env.test" })
 
 export default defineConfig<ConfigOptions>({
   testDir: "./tests/e2e", // Set your test directory
@@ -9,4 +12,20 @@ export default defineConfig<ConfigOptions>({
       rootDir: fileURLToPath(new URL(".", import.meta.url)),
     },
   },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
 })
