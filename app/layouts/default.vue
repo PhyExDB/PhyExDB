aeec47d9aa3e07fbb86702db7ab9a7df20072443
niklhut <49069026+niklhut@users.noreply.github.com>
2024-12-06 13:30:22 +0100

    Give header max width

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="min-h-screen flex flex-col">
     <div class="border-b">
-      <div class="flex h-16 items-center ">
-        <NavigationHeader class="mx-6" />
+      <div class="flex container h-16 items-center">
+        <NavigationHeader />
       </div>
     </div>
 
     <!-- Main Content -->
-    <main class="flex-grow container mx-auto px-6 py-6">
+    <main class="flex-grow container mx-auto py-6">
       <NuxtPage />
     </main>
 
