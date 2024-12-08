0c4cc13d52d489ee61ab6310d83130a596c4f25c
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2024-12-09 00:44:42 +0100

    prisma logging

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,41 @@
 import { PrismaClient } from "@prisma/client"
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  const prisma = new PrismaClient({
+    log: [
+      {
+        emit: 'event',
+        level: 'query',
+      },
+      {
+        emit: 'event',
+        level: 'error',
+      },
+      {
+        emit: 'event',
+        level: 'info',
+      },
+      {
+        emit: 'event',
+        level: 'warn',
+      },
+    ],
+  })
+  
+  prisma.$on('query', (e) => {
+    dbLogger.debug(e.query, { params: e.params })
+  })
+  prisma.$on('error', (e) => {
+    dbLogger.error(e.message)
+  })
+  prisma.$on('info', (e) => {
+    dbLogger.info(e.message)
+  })
+  prisma.$on('warn', (e) => {
+    dbLogger.warn(e.message)
+  })
+  
+  return prisma
 }
 
 declare const globalThis: {
