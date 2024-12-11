784e02b221ec7f1107513481ed2385bd5584c611
niklhut <49069026+niklhut@users.noreply.github.com>
2024-12-11 13:09:25 +0100

    Add legal pages

---
+++ app/pages/terms-of-service.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import type { LegalDocumentDetail } from "~~/shared/types"
+
+const { data: termsOfService } = await useAPI<LegalDocumentDetail>("/api/legal/imprint")
+</script>
+
+<template>
+  <div class="prose dark:prose-invert">
+    <h1>
+      {{ termsOfService!.name }}
+    </h1>
+    <p>{{ termsOfService!.text }}</p>
+  </div>
+</template>
