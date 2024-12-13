8155092dd5589aa07e61104f62e55f90e43140bf
LeviAchenbach <149683881+LeviAchenbach@users.noreply.github.com>
2024-12-13 11:49:46 +0100

    Added user tests

---
+++ tests/integration/server/api/users/index.get.test.ts
@@ -0,0 +1,53 @@
+import { describe, expect, it, vi, afterEach } from "vitest"
+import { setup, $fetch } from "@nuxt/test-utils/e2e"
+
+describe("Api Route /api/users", async () => {
+  await setup()
+  afterEach(() => {
+    vi.resetAllMocks()
+  })
+
+  it("Should return a list of users", async () => {
+    const response = await $fetch("/api/users")
+    // Expected output structure
+    const expectedStructure = [
+      { username: "Admin", role: "ADMIN", verified: true },
+      { username: "Moderator", role: "MODERATOR", verified: true },
+      { username: "User", role: "USER", verified: true },
+    ]
+
+    // Ensure data has length of at least 3 (migration with seeds work)
+    expect(response.length).toBeGreaterThan(3)
+
+    // Expect all listed elements
+    expectedStructure.forEach((expectedItem) => {
+      expect(response).toContainEqual(expect.objectContaining(expectedItem))
+    })
+    response.forEach((item) => {
+      // Expect a UUID for each item
+      expect(item).toHaveProperty("id")
+      // Expect the list items to have a name
+      expect(item).toHaveProperty("username")
+      // Expect the list items to have a role
+      expect(item).toHaveProperty("role")
+      expect(item.id).toMatch(
+        /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+      )
+      expect(item).toHaveProperty("verified")
+    })
+  })
+
+  it("Should return users with correct roles", async () => {
+    const response = await $fetch("/api/users")
+    response.forEach((item) => {
+      expect(["USER", "MODERATOR", "ADMIN"]).toContain(item.role)
+    })
+  })
+
+  it("Should return users with verified status as a boolean", async () => {
+    const response = await $fetch("/api/users")
+    response.forEach((item) => {
+      expect([true, false]).toContain(item.verified)
+    })
+  })
+})
