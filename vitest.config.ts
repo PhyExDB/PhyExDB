eefb44cd6bc5d8b9c02ff776c8beeacce3629d5e
niklhut <49069026+niklhut@users.noreply.github.com>
2024-12-13 15:17:07 +0100

    Add code coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,15 @@ import { defineVitestConfig } from "@nuxt/test-utils/config"
 
 export default defineVitestConfig({
   test: {
+    root: "./",
     dir: "./tests/",
     exclude: ["**/e2e/**"],
     environment: "nuxt",
     setupFiles: ["./server/utils/prisma.ts"],
+    coverage: {
+      reportsDirectory: "./coverage",
+      provider: "istanbul",
+      include: ["**/server/**", "**/app/**", "**/shared/**"],
+    },
   },
 })
