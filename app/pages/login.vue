756e3bab720e9c3bd9c154df9f64a928e7c476b7
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2024-12-14 02:43:33 +0100

    add Form component

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
+import { toTypedSchema } from "@vee-validate/valibot"
+import { useForm } from "vee-validate"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+// import { userLoginSchema } from "~~/shared/types/User.type"
 
 definePageMeta({
   title: "Login",
   description: "Login to your account",
   layout: "auth",
 })
+
+const loading = ref(false)
+
+const formSchema = toTypedSchema(userLoginSchema)
+
+async function signIn(email: string, password: string) {
+  if (loading.value) return
+  loading.value = true
+  console.log("hi", email, password)
+  // const { error } = await authClient.signIn.email({
+  //   email: email.value,
+  //   password: password.value,
+  // })
+  // if (error) {
+  //   console.log(error)
+  //   // toast.add({
+  //   //   title: error.message,
+  //   //   color: 'red',
+  //   // })
+  // } else {
+  //   await navigateTo("/user")
+  //   // toast.add({
+  //   //   title: `You have been signed in!`,
+  //   // })
+  // }
+  loading.value = false
+}
 </script>
 
 <template>
@@ -22,10 +52,60 @@ definePageMeta({
       </CardDescription>
     </CardHeader>
     <CardContent class="grid gap-4">
+      <Form
+        class="w-2/3 space-y-6"
+        @submit="signIn"
+      >
+        <FormField
+          v-slot="{ componentField }"
+          name="email"
+        >
+          <FormItem>
+            <FormLabel>E-mail</FormLabel>
+            <FormControl>
+              <Input
+                id="email"
+                v-bind="componentField"
+                type="email"
+                required
+              />
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <FormField
+          v-slot="{ componentField }"
+          name="password"
+        >
+          <FormItem>
+            <FormLabel>Passwort</FormLabel>
+            <FormControl>
+              <Input
+                id="password"
+                v-bind="componentField"
+                type="password"
+                required
+              />
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+        <Button type="submit">
+          Submit
+        </Button>
+      </form>
+
       <div class="grid gap-2">
         <Label for="email">E-Mail oder Nutzername</Label>
         <Input
           id="email"
+          v-model="email"
           type="email"
           required
         />
@@ -34,11 +114,17 @@ definePageMeta({
         <Label for="password">Passwort</Label>
         <Input
           id="password"
+          v-model="password"
           type="password"
           required
         />
       </div>
-      <Button class="w-full">
+      <Button
+        class="w-full"
+        loading="loading"
+        type="button"
+        @click="signIn"
+      >
         Anmelden
       </Button>
       <div class="text-center text-sm">
