fbe78aae53495284952d70346e95df090c86363b
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2024-12-15 17:02:51 +0100

    basic backend

--- server/plugins/authorization-resolver.ts
+++ server/plugins/authorization-resolver.ts
@@ -1,8 +1,11 @@
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook("request", async (event) => {
     event.context.$authorization = {
-      resolveServerUser: () => {
-        return event.context.user
+      resolveServerUser: async () => {
+        const session = await auth.api.getSession({
+          headers: event.headers,
+        })
+        return session?.user
       },
     }
   })
