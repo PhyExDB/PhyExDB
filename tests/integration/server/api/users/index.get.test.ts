20479d0af787aa9049df28b91b7f700e740b9179
LeviAchenbach <149683881+LeviAchenbach@users.noreply.github.com>
2024-12-16 16:35:38 +0100

    Deleted unneccessary import

--- tests/integration/server/api/users/index.get.test.ts
+++ tests/integration/server/api/users/index.get.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, expectTypeOf } from "vitest"
+import { describe, expect, it } from "vitest"
 import { setup, $fetch } from "@nuxt/test-utils/e2e"
 
 describe("Api Route /api/users", async () => {
