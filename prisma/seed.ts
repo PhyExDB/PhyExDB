00c229618cf7e6b5d63f52d648cc349e40554181
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2024-12-16 16:29:22 +0100

    seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,37 +1,64 @@
 import { PrismaClient } from "@prisma/client"
+import { v4 as uuidv4 } from "uuid"
 
 const prisma = new PrismaClient()
 
 async function userMigrations() {
-  // await prisma.user.create({
-  //   data: {
-  //     email: "user@test.test",
-  //     name: "User",
-  //     role: "USER",
-  //     emailVerified: true,
-  //     accounts: {
-  //       create: {
-  //     }
-  //   },
-  // })
-  // await prisma.user.create({
-  //   data: {
-  //     email: "moderator@test.test",
-  //     name: "Moderator",
-  //     role: "MODERATOR",
-  //     passwordHash: await hash("moderator"),
-  //     emailVerified: true,
-  //   },
-  // })
-  // await prisma.user.create({
-  //   data: {
-  //     email: "admin@test.test",
-  //     name: "Admin",
-  //     role: "ADMIN",
-  //     passwordHash: await hash("admin"),
-  //     emailVerified: true,
-  //   },
-  // })
+  await prisma.account.create({
+    data: {
+      /* password: password */
+      password: "d5e0f1c480664b5cebf349bb252b2d95:d8b59da61839c29ff838df0df6b00082c21f8c999de07467b71cd19fc2dedd519042d6d0b6b043d0ac3367dec911c9ba54fd7acb4e6758159a034d4b2f713c40",
+      providerId: "credential",
+      accountId: uuidv4(),
+      id: uuidv4(),
+      user: {
+        create: {
+          role: "USER",
+          name: "User",
+          email: "user@test.test",
+          emailVerified: true,
+        },
+      },
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  })
+  await prisma.account.create({
+    data: {
+      password: "d5e0f1c480664b5cebf349bb252b2d95:d8b59da61839c29ff838df0df6b00082c21f8c999de07467b71cd19fc2dedd519042d6d0b6b043d0ac3367dec911c9ba54fd7acb4e6758159a034d4b2f713c40",
+      providerId: "credential",
+      accountId: uuidv4(),
+      id: uuidv4(),
+      user: {
+        create: {
+          role: "MODERATOR",
+          name: "Moderator",
+          email: "moderator@test.test",
+          emailVerified: true,
+        },
+      },
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  })
+  await prisma.account.create({
+    data: {
+      password: "d5e0f1c480664b5cebf349bb252b2d95:d8b59da61839c29ff838df0df6b00082c21f8c999de07467b71cd19fc2dedd519042d6d0b6b043d0ac3367dec911c9ba54fd7acb4e6758159a034d4b2f713c40",
+      providerId: "credential",
+      accountId: uuidv4(),
+      id: uuidv4(),
+      user: {
+        create: {
+          role: "ADMIN",
+          name: "Admin",
+          email: "admin@test.test",
+          emailVerified: true,
+        },
+      },
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  })
 }
 
 async function legalMigrations() {
