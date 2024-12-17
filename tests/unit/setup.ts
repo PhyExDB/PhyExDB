37bb0097396d3204a0a40bb268c164fe090856a4
niklhut <49069026+niklhut@users.noreply.github.com>
2024-12-17 15:23:28 +0100

    Improve global setup

--- tests/unit/setup.ts
+++ tests/unit/setup.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { PrismaClient } from "@prisma/client"
 import { vitest } from "vitest"
 import { mockDeep } from "vitest-mock-extended"
@@ -5,8 +6,8 @@ import { mockDeep } from "vitest-mock-extended"
 vitest.stubGlobal("defineEventHandler", (func: unknown) => func)
 vitest.stubGlobal("defineRouteMeta", (func: unknown) => func)
 vitest.stubGlobal("defineNitroPlugin", (e: unknown) => e)
-vitest.stubGlobal("getRouterParam", (e: unknown) => e.context.params.slug)
-vitest.stubGlobal("getValidatedRouterParams", (e: unknown, f: unknown) => f(e.context.params))
+vitest.stubGlobal("getRouterParam", (e: any) => e.context.params.slug)
+vitest.stubGlobal("getValidatedRouterParams", (e: any, f: any) => f(e.context.params))
 
 const prisma = mockDeep<PrismaClient>()
 vitest.stubGlobal("prisma", prisma)
