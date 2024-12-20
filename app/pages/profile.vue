3c7f2f5c5e14661cdbf88094f9c88157a98d8352
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2024-12-20 11:00:09 +0100

    Card

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -5,6 +5,7 @@ import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { emailSchema } from "~~/shared/types/User.type"
+import getInitials from "~~/shared/utils/initials"
 
 definePageMeta({
   title: "Profile",
@@ -53,58 +54,127 @@ const onSubmit = form.handleSubmit(async (values) => {
 
 <template>
   <div v-if="user">
-    emailVerified: {{ user.emailVerified }}
-    <Button v-if="user.emailVerified">
-      E-Mail Verifizieren
-    </Button>
+    <Card class="max-w-sm w-full">
+      <CardContent class="grid gap-4">
+        <Avatar>
+          <AvatarFallback>{{ getInitials(user.username) }}</AvatarFallback>
+        </Avatar>
+        <p
+          order-1
+          md:order-1
+        >
+          {{ user.username }}
+        </p>
+        <p
+          order-2
+          md:order-3
+          color:muted
+        >
+          {{ user.email }}
+        </p>
+        <Dialog
+          order-3
+          md:order-2
+        >
+          <DialogTrigger as-child>
+            <Button variant="outline">
+              Edit Profile
+            </Button>
+          </DialogTrigger>
+          <DialogContent class="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            fhgfhg
+            <DialogFooter>
+              <Button type="submit">
+                Save changes
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+        <Dialog
+          order-4
+          md:order-4
+        >
+          <DialogTrigger as-child>
+            <Button variant="outline">
+              Edit Profile
+            </Button>
+          </DialogTrigger>
+          <DialogContent class="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            fhgfhg
+            <DialogFooter>
+              <Button type="submit">
+                Save changes
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
 
-    <form
-      class="w-2/3 space-y-6"
-      @submit="onSubmit"
-    >
-      <FormField
-        v-slot="{ componentField }"
-        name="email"
-      >
-        <FormItem>
-          <FormLabel>E-mail</FormLabel>
-          <FormControl>
-            <Input
-              id="email"
-              v-bind="componentField"
-              type="email"
-              :placeholder="user.email"
-            />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      </FormField>
-      <FormField
-        v-slot="{ componentField }"
-        name="username"
-      >
-        <FormItem>
-          <FormLabel>Username</FormLabel>
-          <FormControl>
-            <Input
-              id="username"
-              v-bind="componentField"
-              type="string"
-              :placeholder="user.username"
-            />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      </FormField>
-      <Button
-        :loading="loading"
-        type="submit"
-      >
-        Submit
-      </Button>
-    </form>
-    <Button>
-      Passwort Ändern
-    </Button>
+        <!-- emailVerified: {{ user.emailVerified }}
+        <Button v-if="user.emailVerified">
+          E-Mail Verifizieren
+        </Button>
+
+        <form
+          class="w-2/3 space-y-6"
+          @submit="onSubmit"
+        >
+          <FormField
+            v-slot="{ componentField }"
+            name="email"
+          >
+            <FormItem>
+              <FormLabel>E-mail</FormLabel>
+              <FormControl>
+                <Input
+                  id="email"
+                  v-bind="componentField"
+                  type="email"
+                  :placeholder="user.email"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+          <FormField
+            v-slot="{ componentField }"
+            name="username"
+          >
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input
+                  id="username"
+                  v-bind="componentField"
+                  type="string"
+                  :placeholder="user.username"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+          <Button
+            :loading="loading"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </form>
+        <Button>
+          Passwort Ändern
+        </Button> -->
+      </CardContent>
+    </Card>
   </div>
 </template>
