32e7e854a4c05e519d0cc51d83f057b0582232a5
niklhut <49069026+niklhut@users.noreply.github.com>
2024-12-21 20:28:24 +0100

    Fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,13 +9,13 @@ export default defineConfig<ConfigOptions>({
   timeout: 60000,
   webServer: {
     command: "npm run build && npm run preview",
-    url: "http://127.0.0.1:3000",
+    url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     stdout: "ignore",
     stderr: "pipe",
   },
   use: {
-    baseURL: "http://127.0.0.1:3000",
+    baseURL: "http://localhost:3000",
   },
   projects: [
     {
