14de94eb98b75f0d40b6799ab277a32365c749ea
niklhut <49069026+niklhut@users.noreply.github.com>
2024-12-21 15:46:17 +0100

    Add login and register tests

---
+++ tests/e2e/app/pages/login.test.ts
@@ -0,0 +1,80 @@
+import { test, expect } from "@nuxt/test-utils/playwright"
+import { validateFooter } from "~~/tests/helpers/validateFooter"
+
+test.describe("Login Page", () => {
+  test("should have link to register page", async ({ page, goto }) => {
+    await goto("/login", { waitUntil: "hydration" })
+    await expect(page.getByRole("main")).toContainText("Registrieren")
+    await expect(page.getByRole("link", { name: "Registrieren" })).toHaveAttribute("href", "/register")
+  })
+
+  test("should validate email", async ({ page, goto }) => {
+    await goto("/login", { waitUntil: "hydration" })
+    await page.locator("#email").click()
+    await page.locator("#email").fill("hallo")
+    await expect(page.locator("form")).toMatchAriaSnapshot(`
+      - text: E-Mail
+      - textbox: hallo
+      - alert: Invalide E-Mail-Adresse
+      `)
+    await page.locator("#email").click()
+    await page.locator("#email").fill("hallo@test.test")
+    await expect(page.locator("form")).toMatchAriaSnapshot(`
+      - text: E-Mail
+      - textbox: hallo@test.test
+      `)
+  })
+
+  test("should display errors and work correctly", async ({ page, goto }) => {
+    await goto("/login", { waitUntil: "hydration" })
+    await validateFooter(page)
+    await expect(page.getByRole("main")).toMatchAriaSnapshot(`
+      - main:
+        - heading "Anmelden" [level=3]
+        - paragraph: Gib deine E-Mail Adresse oder deinen Benutzernamen und dein Passwort ein.
+        - text: E-Mail
+        - textbox
+        - text: Passwort
+        - textbox
+        - button "Anmelden"
+        - text: Noch kein Account?
+        - link "Registrieren"
+    `)
+    await page.getByRole("button", { name: "Anmelden" }).click()
+    await expect(page.getByRole("main")).toMatchAriaSnapshot(`
+      - main:
+        - heading "Anmelden" [level=3]
+        - paragraph: Gib deine E-Mail Adresse oder deinen Benutzernamen und dein Passwort ein.
+        - text: E-Mail
+        - textbox
+        - alert: E-Mail-Adresse muss angegeben werden
+        - text: Passwort
+        - textbox
+        - alert: Passwort muss angegeben werden
+        - button "Anmelden"
+        - text: Noch kein Account?
+        - link "Registrieren"
+    `)
+    await page.locator("#email").click()
+    await page.locator("#email").fill("user@test.test")
+    await page.locator("#password").click()
+    await page.locator("#password").fill("password")
+    await expect(page.getByRole("main")).toMatchAriaSnapshot(`
+      - main:
+        - heading "Anmelden" [level=3]
+        - paragraph: Gib deine E-Mail Adresse oder deinen Benutzernamen und dein Passwort ein.
+        - text: E-Mail
+        - textbox: user@test.test
+        - text: Passwort
+        - textbox: password
+        - button "Anmelden"
+        - text: Noch kein Account?
+        - link "Registrieren"
+    `)
+    await page.getByRole("button", { name: "Anmelden" }).click()
+    await expect(page).toHaveURL("/profile")
+
+    await goto("/login", { waitUntil: "hydration" })
+    await expect(page).toHaveURL("/profile")
+  })
+})
