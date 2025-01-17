9bd86c99cb604dc87237be5c1deea69db00f7ec5
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-01-17 12:42:01 +0100

    own authorize impl

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { H3Event, EventHandlerRequest } from "h3"
 import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
@@ -57,4 +58,58 @@ export async function getUserOrThrowError(event: H3Event<EventHandlerRequest>): 
     throw createError({ statusCode: 401, statusMessage: "Not logged in" })
   }
   return user
+}
+
+type AbillityFunc<T extends any[]> = (user: UserDetail, ...param: T) => boolean
+type Abillity<T extends any[]> = { func: AbillityFunc<T>, allowGuests: boolean }
+type UserAbillity<T extends any[]> = { func: AbillityFunc<T>, allowGuests: false }
+
+/**
+ * Authorizes a user based on the provided ability and parameters.
+ */
+export async function authorize<T extends any[]>(
+  event: H3Event<EventHandlerRequest>,
+  abillity: Abillity<T>,
+  ...param: T
+): Promise<UserDetail | null> {
+  const user = await getUser(event)
+  if (!user) {
+    if (abillity.allowGuests) {
+      return null
+    } else {
+      throw createError({ statusCode: 401, statusMessage: "Not logged in" })
+    }
+  }
+  const isAuthorized = abillity.func(user, ...param)
+  if (!isAuthorized) {
+    throw createError({ statusCode: 403, statusMessage: "Not authorized" })
+  }
+  return user
+}
+/**
+ * Authorizes a user based on the provided UserAbility and parameters. Can't be used with abillities to prevent the strengthening of requirements.
+ */
+export async function authorizeUser<T extends any[]>(
+  event: H3Event<EventHandlerRequest>,
+  abillity: UserAbillity<T>,
+  ...param: T
+): Promise<UserDetail> {
+  const user = await getUserOrThrowError(event)
+  const isAuthorized = abillity.func(user, ...param)
+  if (!isAuthorized) {
+    throw createError({ statusCode: 403, statusMessage: "Not authorized" })
+  }
+  return user
+}
+/**
+ * Defines an ability.
+ */
+export function defineAbillity<T extends any[]>(allowGuests: boolean, func: AbillityFunc<T>): Abillity<T> {
+  return { func, allowGuests }
+}
+/**
+ * Defines an userAbility.
+ */
+export function defineUserAbillity<T extends any[]>(func: AbillityFunc<T>): UserAbillity<T> {
+  return { func, allowGuests: false }
 }
