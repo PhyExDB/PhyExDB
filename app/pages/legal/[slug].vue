97d65b77e50b08d63ccce6b57203959046760c7e
Keller, Finn <finn.keller@stud.tu-darmstadt.de>
2025-01-17 13:16:56 +0100

    no auth stuff yet

--- app/pages/legal/[slug].vue
+++ app/pages/legal/[slug].vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-import Button from "~/components/ui/button/Button.vue"
+import { toTypedSchema } from "@vee-validate/zod"
+import { useForm } from "vee-validate"
 import type { LegalDocumentDetail } from "~~/shared/types"
 
 definePageMeta({
@@ -7,32 +8,101 @@ definePageMeta({
     return params.slug === "imprint" || params.slug === "privacy-policy" || params.slug === "terms-of-service"
   },
 })
+const loading = ref(false)
+const open = ref(false)
+const formSchema = toTypedSchema(legalDocumentUpdateSchema)
+const form = useForm({ validationSchema: formSchema })
 
 const route = useRoute()
 const slug = route.params.slug as string
 
 const { data: legal } = await useAPI<LegalDocumentDetail>(`/api/legal/${slug}`)
+
+const onSubmit = form.handleSubmit(async (values) => {
+  if (loading.value) return
+  legal.value = await $fetch(`/api/legal/${slug}`, {
+    method: "PUT",
+    body: values,
+  })
+  open.value = false
+})
 </script>
 
 <template>
-  <div class="prose dark:prose-invert">
-    <h1>
-      {{ legal!.name }}
-    </h1>
-    <p>{{ legal!.text }}</p>
-    <Can
-      :ability="onlyAdminAbillity"
+  <div>
+    <div class="prose dark:prose-invert">
+      <h1>
+        {{ legal!.name }}
+      </h1>
+      <p>{{ legal!.text }}</p>
+    </div>
+    <Dialog
+      :open="open"
+      @update:open="open=$eve"
     >
-      <Button>
-        <div>
-          <NuxtLink
-            to="EditLegal"
-            class="text-black"
+      <DialogTrigger as-child>
+        <Button
+          variant="outline"
+          class="m-1 px-5 justify-center"
+          @click="open=true"
+        >
+          edit
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Ändern des Dokuments
+          </DialogTitle>
+        </DialogHeader>
+        <form
+          class="grid gap-4"
+          @submit="onSubmit"
+        >
+          <FormField
+            v-slot="{ componentField }"
+            name="name"
           >
-            edit
-          </NuxtLink>
-        </div>
-      </button>
-    </Can>
+            <FormItem>
+              <FormLabel>Text</FormLabel>
+              <FormControl>
+                <Input
+                  id="name"
+                  :default-value="legal?.name"
+                  v-bind="componentField"
+                  type="text"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+          <FormField
+            v-slot="{ componentField }"
+            name="text"
+          >
+            <FormItem>
+              <FormLabel>Text</FormLabel>
+              <FormControl>
+                <Input
+                  id="text"
+                  :default-value="legal?.text"
+                  v-bind="componentField"
+                  type="text"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+        </form>
+        <DialogFooter>
+          <Button
+            type="submit"
+            @click="onSubmit"
+          >
+            Speichern
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   </div>
 </template>
