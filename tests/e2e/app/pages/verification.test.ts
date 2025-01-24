0acb57f40e1563ae2b5d7db5adc22ac54575564f
LeviAchenbach <149683881+LeviAchenbach@users.noreply.github.com>
2025-01-24 14:51:10 +0100

    E2E Test for verification of user done

--- tests/e2e/app/pages/verification.test.ts
+++ tests/e2e/app/pages/verification.test.ts
@@ -1,34 +1,50 @@
 import { test, expect } from "@playwright/test"
 import { v4 as uuidv4 } from "uuid"
 
-test("Test verification working", async ({ page }) => {
+test("test", async ({ page }) => {
   const id = uuidv4()
   const name = `user-${id}`
-  const email = `${name}@test.test`
+  const email = `${id}@test.test`
+  const emailRegex = new RegExp(`^${id}@test\\.test$`)
 
-  await page.goto("/register", { waitUntil: "networkidle" })
+  await page.goto("http://localhost:3000/register", { waitUntil: "networkidle" })
   await page.locator("#email").click()
   await page.locator("#email").fill(email)
-  await page.locator("#email").press("Tab")
+  await page.locator("#name").click()
   await page.locator("#name").fill(name)
-  await page.locator("#name").press("Tab")
+  await page.locator("#password").click()
   await page.locator("#password").fill("1Passwort")
-  await page.locator("#password").press("Tab")
+  await page.locator("#confirmPassword").click()
   await page.locator("#confirmPassword").fill("1Passwort")
   await page.getByLabel("Ich akzeptiere die").click()
   await page.getByRole("button", { name: "Registrieren" }).click()
-  await page.goto("http://localhost:8025/")
-  await page.getByRole("link", { name: `email@email.com To: ${name}` }).click()
-  const page1Promise = page.waitForEvent("popup")
-  await page.getByRole("link", { name: "http://localhost:3000/api/" }).click()
-  const page1 = await page1Promise
-  await page1.getByRole("button", { name: "US" }).click()
-  await page1.getByRole("link", { name: "Profil" }).click()
-  await expect(page.locator("div").filter({ hasText: email }).locator("span")).toBeVisible()
-  await page.locator("div").filter({ hasText: email }).locator("span").click()
+  await expect(page.locator("div").filter({ hasText: emailRegex }).locator("span")).toBeVisible()
+  await page.locator("div").filter({ hasText: emailRegex }).locator("span").click()
   await expect(page.locator("[id^='radix-vue-popover-content']")).toBeVisible()
   await expect(page.locator("[id^='radix-vue-popover-content']")).toMatchAriaSnapshot(`
       - dialog:
-        - paragraph: E-Mail ist verifiziert
+        - paragraph: E-Mail ist nicht verifiziert
+        - button "E-Mail verifizieren"
       `)
+
+  await page.goto("http://localhost:8025/", { waitUntil: "networkidle" })
+  await page.getByRole("link", { name: `email@email.com To: ${email}` }).click()
+  const page1Promise = page.waitForEvent("popup")
+  await page.getByRole("link", { name: "http://localhost:3000/api/" }).click()
+  const page1 = await page1Promise
+  await page1.goto("/profile", { waitUntil: "networkidle" })
+  await expect(page1.getByRole("main")).toMatchAriaSnapshot(`
+    - text: US
+    - paragraph: ${name}
+    - paragraph: ${email}
+    - button "Name oder E-Mail ändern"
+    - button "Passwort ändern"
+    `)
+  await expect(page1.locator("div").filter({ hasText: emailRegex }).locator("span")).toBeVisible()
+  await page1.locator("div").filter({ hasText: emailRegex }).locator("span").click()
+  await expect(page1.locator("[id^='radix-vue-popover-content']")).toBeVisible()
+  await expect(page1.locator("[id^='radix-vue-popover-content']")).toMatchAriaSnapshot(`
+    - dialog:
+      - paragraph: E-Mail ist verifiziert
+    `)
 })
