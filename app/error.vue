700b38ac9e3afdb307f68f9214e27e9721549370
Tim Kroker <Tim-Kroker@gmx.net>
2025-01-28 19:06:00 +0100

    custom error page

---
+++ app/error.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app"
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="flex flex-col items-center space-y-4">
+      <h1 class="text-4xl">
+        Etwas ist schiefgelaufen...
+      </h1>
+      <div v-if="props.error?.statusCode">
+        <div class="flex text-xl text-gray-400">
+          <p>
+            Fehler {{ props.error.statusCode }}
+          </p>
+          <p>
+            : {{ props.error.message }}
+          </p>
+        </div>
+      </div>
+      <Button>
+        <NuxtLink to="/">Zurück zur Startseite</NuxtLink>
+      </Button>
+    </div>
+  </div>
+</template>
