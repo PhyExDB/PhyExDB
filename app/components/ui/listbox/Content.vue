2f1f2cc47c404002bda6871c31f36da4228c8e5e
niklhut <49069026+niklhut@users.noreply.github.com>
2025-01-28 17:47:10 +0100

    Add listbox component

---
+++ app/components/ui/listbox/Content.vue
@@ -0,0 +1,22 @@
+<template>
+  <ListboxContent
+    v-bind="forwarded"
+    :class="styles({ class: props.class })"
+  >
+    <slot />
+  </ListboxContent>
+</template>
+
+<script lang="ts" setup>
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ListboxContent, useForwardProps } from "radix-vue"
+import type { ListboxContentProps } from "radix-vue"
+import { cva } from "class-variance-authority"
+
+const props = defineProps<ListboxContentProps & { class?: any }>()
+const forwarded = useForwardProps(reactiveOmit(props))
+
+const styles = cva(
+  "max-h-[300px] w-full overflow-y-auto rounded-md border bg-popover px-4 py-2",
+)
+</script>
---
+++ app/components/ui/listbox/GroupLabel.vue
@@ -0,0 +1,20 @@
+<template>
+  <ListboxGroupLabel
+    v-bind="forwarded"
+    :class="styles({ class: props.class })"
+  >
+    <slot />
+  </ListboxGroupLabel>
+</template>
+
+<script lang="ts" setup>
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { cva } from "class-variance-authority"
+import { ListboxGroupLabel, useForwardProps } from "radix-vue"
+import type { ListboxGroupLabelProps } from "radix-vue"
+
+const props = defineProps<ListboxGroupLabelProps & { class?: any }>()
+const forwarded = useForwardProps(reactiveOmit(props, "class"))
+
+const styles = cva("py-1.5 pl-1 pr-8 text-sm font-semibold")
+</script>
