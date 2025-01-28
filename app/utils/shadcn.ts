2aa5e2f930cc577f4060a0ed5788b6512c68d42d
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-01-28 09:58:32 +0100

    teamscale

--- app/utils/shadcn.ts
+++ app/utils/shadcn.ts
@@ -4,10 +4,19 @@ import type { Ref } from "vue"
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Merges multiple class names into a single string.
+ *
+ * This function takes any number of class name inputs, processes them using `clsx`,
+ * and then merges them using `twMerge` to ensure Tailwind CSS classes are combined correctly.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+/**
+ * Updates the value of a given reference based on the provided updater function or value.
+ */
 export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref) {
   ref.value = typeof updaterOrValue === "function"
     ? updaterOrValue(ref.value)
