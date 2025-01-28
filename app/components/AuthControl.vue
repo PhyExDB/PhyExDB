2b63f0faabe796df8528f87e4fb606d61673528e
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-01-28 07:33:20 +0100

    link from authControl

--- app/components/AuthControl.vue
+++ app/components/AuthControl.vue
@@ -8,6 +8,8 @@ async function stopImpersonating() {
   await useAuth().client.admin.stopImpersonating()
   await navigateTo("/users")
 }
+
+const allowed = await allows(userAbilities.getAll)
 </script>
 
 <template>
@@ -23,15 +25,23 @@ async function stopImpersonating() {
           <span>Profil</span>
         </NuxtLink>
       </DropdownMenuItem>
-      <DropdownMenuItem @click="useAuth().client.signOut()">
-        <span>Abmelden</span>
-      </DropdownMenuItem>
-
+      
       <DropdownMenuItem
         v-if="data?.session.impersonatedBy"
         @click="stopImpersonating"
       >
         <span>Imitieren beenden</span>
+      </DropdownMenuItem>
+      <DropdownMenuItem
+        v-if="allowed"
+      >
+        <NuxtLink href="/users">
+          <span>Nutzerverwaltung</span>
+        </NuxtLink>
+      </DropdownMenuItem>
+
+      <DropdownMenuItem @click="useAuth().client.signOut()">
+        <span>Abmelden</span>
       </DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
