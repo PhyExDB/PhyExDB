6459b87cb4977990daafc11ca401e56f52b3e9f8
niklhut <49069026+niklhut@users.noreply.github.com>
2025-01-29 21:10:30 +0100

    Squash migrations, fixes #274

--- prisma/migrations/20250129174807_add_seed_status/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "SeedStatus" (
-    "id" TEXT NOT NULL,
-    "seedName" TEXT NOT NULL,
-    "performedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "SeedStatus_pkey" PRIMARY KEY ("id")
-);
