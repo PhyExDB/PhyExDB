f424303677431cbe20b08466321d061a74697aa4
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-02-04 20:18:37 +0100

    refactoring

--- tests/unit/server/api/legal/[slug].put.test.ts
+++ tests/unit/server/api/legal/[slug].put.test.ts
@@ -23,113 +23,105 @@ const workingParams = []
 const failingParams = []
 
 describe("Api Route PUT /api/legal/{slug}", () => {
-
-  ["slug", "id of"].forEach((param) => {
+  it(`should_succeed`, async () => {
+    // definitions
+    const body = generateMock(legalDocumentUpdateSchema)
     
-    it.each([
-      { slug: "privacy-policy" },
-      { slug: "terms-of-service" },
-      { slug: "imprint" },
-    ])(`should_succeed with ${param} $slug`, async ({ slug }) => {
-      // definitions
-      const body = generateMock(legalDocumentUpdateSchema)
-      
-      const data = {
-        id: uuidv4(),
-        slug: slug,
-        name: "Legal Document name",
-        text: "This is the legal document text",
-      }
-      const expected = { ...data, ...body }
-      
-      const event = getEvent({ params: { slug }, body: body })
-
-      // mock
-      mockPrismaForPostSlugOrId("legalDocument", data, expected)
-
-      // test
-      const response = await updateLegalDocument(event)
-
-      // assertions
-      expectTypeOf(response).toEqualTypeOf<LegalDocumentDetail>()
-      expect(response).toStrictEqual(expected)
-    })
-  })
-
-  it.each([
-    { value: "name", description: "a name" },
-    { value: "text", description: "some content" },
-  ])(`should return an error when the field $value empty`, async ({ value, description }) => {
-    const updateContent = {
-      name: "New name",
-      text: "New content",
-    }
-    if (value === "name") {
-      updateContent.name = ""
-    } else if (value === "text") {
-      updateContent.text = ""
-    }
-
-    const event = {
-      context: {
-        params: {
-          slug: "privacy-policy",
-        },
-      },
-      body: updateContent,
-    } as unknown as H3Event<EventHandlerRequest>
-
-    const document = {
+    const data = {
       id: uuidv4(),
-      slug: "privacy-policy",
+      slug: "legal-document",
       name: "Legal Document name",
       text: "This is the legal document text",
     }
+    const expected = { ...data, ...body }
+    
+    const event = getEvent({ params: { slug }, body: body })
 
-    const getter = vi.fn().mockImplementation(({ where }) => {
-      if (where.id === document.id || where.slug === document.slug) {
-        return Promise.resolve(document)
-      }
-      return Promise.resolve(null)
-    })
+    // mock
+    mockPrismaForPostSlugOrId("legalDocument", data, expected)
 
-    prisma.legalDocument.findFirst = getter
-    prisma.legalDocument.update = getter
+    // test
+    const response = await updateLegalDocument(event)
 
-    await expect(updateLegalDocument(event)).rejects.toThrowError(
-      expect.objectContaining({
-        message: expect.stringContaining(`Please enter ${description}`),
-      }),
-    )
+    // assertions
+    expectTypeOf(response).toEqualTypeOf<LegalDocumentDetail>()
+    expect(response).toStrictEqual(expected)
+  })
+})
+
+it.each([
+  { value: "name", description: "a name" },
+  { value: "text", description: "some content" },
+])(`should return an error when the field $value empty`, async ({ value, description }) => {
+  const updateContent = {
+    name: "New name",
+    text: "New content",
+  }
+  if (value === "name") {
+    updateContent.name = ""
+  } else if (value === "text") {
+    updateContent.text = ""
+  }
+
+  const event = {
+    context: {
+      params: {
+        slug: "privacy-policy",
+      },
+    },
+    body: updateContent,
+  } as unknown as H3Event<EventHandlerRequest>
+
+  const document = {
+    id: uuidv4(),
+    slug: "privacy-policy",
+    name: "Legal Document name",
+    text: "This is the legal document text",
+  }
+
+  const getter = vi.fn().mockImplementation(({ where }) => {
+    if (where.id === document.id || where.slug === document.slug) {
+      return Promise.resolve(document)
+    }
+    return Promise.resolve(null)
   })
 
-  it ("should return an error when an unknown slug is passed", async () => {
-    prisma.legalDocument.findFirst = vi.fn().mockResolvedValue(null)
+  prisma.legalDocument.findFirst = getter
+  prisma.legalDocument.update = getter
 
-    const event = {
-      context: {
-        params: {
-          slug: uuidv4(),
-        },
+  await expect(updateLegalDocument(event)).rejects.toThrowError(
+    expect.objectContaining({
+      message: expect.stringContaining(`Please enter ${description}`),
+    }),
+  )
+})
+
+it ("should return an error when an unknown slug is passed", async () => {
+  prisma.legalDocument.findFirst = vi.fn().mockResolvedValue(null)
+
+  const event = {
+    context: {
+      params: {
+        slug: uuidv4(),
       },
-    } as unknown as H3Event<EventHandlerRequest>
+    },
+  } as unknown as H3Event<EventHandlerRequest>
 
-    await expect(updateLegalDocument(event)).rejects.toMatchObject({
-      message: "Document not found",
-      statusCode: 404,
-    })
+  await expect(updateLegalDocument(event)).rejects.toMatchObject({
+    message: "Document not found",
+    statusCode: 404,
   })
+})
 
-  it("should return an error when no slug is passed", async () => {
-    const event = {
-      context: {
-        params: {},
-      },
-    } as unknown as H3Event<EventHandlerRequest>
+it("should return an error when no slug is passed", async () => {
+  const event = {
+    context: {
+      params: {},
+    },
+  } as unknown as H3Event<EventHandlerRequest>
 
-    await expect(updateLegalDocument(event)).rejects.toMatchObject({
-      message: "Invalid slug",
-      statusCode: 400,
-    })
+  await expect(updateLegalDocument(event)).rejects.toMatchObject({
+    message: "Invalid slug",
+    statusCode: 400,
   })
 })
