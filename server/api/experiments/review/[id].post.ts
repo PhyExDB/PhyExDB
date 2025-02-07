2839d8b10b4dc674c375158dd5ac44e28a8f1f27
niklhut <49069026+niklhut@users.noreply.github.com>
2025-02-07 15:03:24 +0100

    Add route meta

--- server/api/experiments/review/[id].post.ts
+++ server/api/experiments/review/[id].post.ts
@@ -25,3 +25,55 @@ export default defineEventHandler(async (event) => {
     },
   })
 })
+
+defineRouteMeta({
+  openAPI: {
+    description: "Approves or rejects an experiment",
+    tags: ["Experiment"],
+    parameters: [
+      {
+        name: "id",
+        in: "path",
+        required: true,
+        description: "The ID of the experiment",
+        schema: {
+          type: "string",
+          format: "uuid",
+        },
+      },
+    ],
+    requestBody: {
+      description: "Review content",
+      required: true,
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              approve: { type: "boolean" },
+              message: { type: "string" },
+            },
+            required: ["approve"],
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: "Experiment submitted successfully",
+      },
+      400: {
+        description: "Invalid slug or ID",
+      },
+      401: {
+        description: "No user is logged in",
+      },
+      403: {
+        description: "Unauthorized",
+      },
+      404: {
+        description: "Experiment not found",
+      },
+    },
+  },
+})
