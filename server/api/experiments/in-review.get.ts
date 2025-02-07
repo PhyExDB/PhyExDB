74e43c69192a92b7b3f02ab75609394974e1c2e1
niklhut <49069026+niklhut@users.noreply.github.com>
2025-02-07 13:22:30 +0100

    Add list reviewable experiments endpoint

---
+++ server/api/experiments/in-review.get.ts
@@ -0,0 +1,93 @@
+export default defineEventHandler(async (event) => {
+  await authorizeUser(event, experimentAbilities.listInReview)
+
+  const totalExperiments = await prisma.experiment.count({
+    where: {
+      status: "IN_REVIEW",
+    },
+  })
+
+  const pageMeta = getPageMeta(event, totalExperiments)
+
+  const experiments = await prisma.experiment.findMany({
+    ...getPaginationPrismaParam(pageMeta),
+    where: {
+      status: "IN_REVIEW",
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+    include: experimentIncludeForToList,
+  })
+
+  return {
+    items: experiments.map(experiment => mapExperimentToList(experiment as ExperimentIncorrectList)),
+    pagination: pageMeta,
+  } as Page<ExperimentList>
+})
+
+defineRouteMeta({
+  openAPI: {
+    description: "Lists all experiments of the current user.",
+    tags: ["Experiment"],
+    responses: {
+      200: {
+        description: "A list of experiments",
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                items: {
+                  type: "array",
+                  items: {
+                    type: "object",
+                    properties: {
+                      id: { type: "string", format: "uuid" },
+                      name: { type: "string" },
+                      slug: { type: "string" },
+                      userId: { type: "string" },
+                      status: {
+                        type: "string",
+                        enum: ["DRAFT", "IN_REVIEW", "PUBLISHED"],
+                      },
+                      duration: { type: "number" },
+                      attributes: {
+                        type: "array",
+                        items: {
+                          type: "object",
+                          properties: {
+                            name: { type: "string" },
+                            id: { type: "string", format: "uuid" },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+                pagination: {
+                  type: "object",
+                  properties: {
+                    total: { type: "integer" },
+                    page: { type: "integer" },
+                    pageSize: { type: "integer" },
+                    totalPages: { type: "integer" },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      400: {
+        description: "Bad Request",
+      },
+      401: {
+        description: "Unauthorized",
+      },
+      403: {
+        description: "Forbidden",
+      },
+    },
+  },
+})
