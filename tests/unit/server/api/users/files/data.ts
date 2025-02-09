39955f1a0bff130f6ca625e7eea6323051eba747
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-02-09 23:51:14 +0100

    users file post

---
+++ tests/unit/server/api/users/files/data.ts
@@ -0,0 +1,14 @@
+import { users } from "~~/tests/helpers/auth"
+import { v4 as uuidv4 } from "uuid"
+
+/** example detail dto */
+export const detail = {
+  id: uuidv4(),
+  file: {
+    id: uuidv4(),
+    path: "/uploads/randomId",
+    mimeType: "text/plain",
+    originalName: "file1",
+  },
+  userId: users.user.id,
+}
---
+++ tests/unit/server/api/users/files/index.post.test.ts
@@ -0,0 +1,53 @@
+import { describe, expectTypeOf, vi } from "vitest"
+import { detail } from "./data"
+import { generateMock } from "@anatine/zod-mock"
+import { mockUser, users } from "~~/tests/helpers/auth"
+import type { EndpointResult } from "~~/tests/helpers/utils"
+import * as u from "~~/tests/helpers/utils"
+import { v4 as uuidv4 } from "uuid"
+
+import endpoint from "~~/server/api/users/files/index.post"
+
+describe("Api Route /api/files/users/index.post", () => {
+  // definitions
+  
+  const data = detail
+  const expected = detail
+
+  const body = {
+    fileId: detail.file.id,
+  }
+  
+  const context = u.getTestContext({
+    data, expected, endpoint,
+
+    body,
+  })
+
+  // mocks
+  mockUser(users.user)
+  u.mockPrismaForIdGet({data: {...detail.file, createdById: users.user.id}}, "file")
+  u.mockPrismaForPost(context, "userFile")
+
+  // tests
+  {
+    // type test
+    expectTypeOf<EndpointResult<typeof endpoint>>().toEqualTypeOf<typeof expected>()
+
+    u.testSuccess(context)
+
+    it("should_fail_when_unknown_fileId", async () => {
+      u.expectErrorObjectMatching(
+        {...context, body: {fileId: uuidv4()}},
+        { message: "File not found" }
+      )
+    })
+    u.testZodFail(context, [
+      {
+        body: { id: "noId" },
+      }
+    ])
+    // needs to be last, because it changes the user mock
+    u.testAuthFail(context, [users.guest])
+  }
+})
