923b7b65a64d1912ee87c6027a4f1a7483c25357
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-02-10 18:35:48 +0100

    experiment submit

---
+++ tests/unit/server/api/experiments/submit/index.post.test.ts
@@ -0,0 +1,48 @@
+import { describe, expectTypeOf, vi } from "vitest"
+import { detail, detailDb } from "../data"
+import { users } from "~~/tests/helpers/auth"
+import type { EndpointResult } from "~~/tests/helpers/utils"
+import * as u from "~~/tests/helpers/utils"
+
+import endpoint from "~~/server/api/experiments/submit/[id].post"
+
+describe("Api Route /api/experiments/submit/[id].post", () => {
+  // definitions
+  const sections: ExperimentSectionList[] = []
+  const attributes: ExperimentAttributeDetail[] = []
+
+  const data = {
+    ...detailDb,
+    sections: [],
+    attributes: [],
+  }
+  const expected: void = undefined
+
+  const context = u.getTestContext({
+    data, expected, endpoint,
+
+    params: { id: data.id },
+    user: users.user,
+  })
+
+  // mocks
+  u.mockPrismaForIdPut({ data: data, expected: data }, "experiment")
+  vi.stubGlobal("$fetch", async (path: string) => {
+    if (path === "/api/experiments/sections") {
+      return sections
+    } else if (path === "/api/experiments/attributes") {
+      return attributes
+    }
+  })
+
+  // tests
+  {
+    // type test
+    expectTypeOf<EndpointResult<typeof endpoint>>().toEqualTypeOf<typeof expected>()
+
+    u.testSuccess(context)
+
+    // needs to be last, because it changes the user mock
+    u.testAuthFail(context, [users.guest, users.unverified])
+  }
+})
