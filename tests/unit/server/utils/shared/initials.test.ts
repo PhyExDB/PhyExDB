6ccf1b19b19e5a0d6eba076ad86aa70b80ea7a0a
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-02-13 16:21:43 +0100

    shared initials

---
+++ tests/unit/server/utils/shared/initials.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, expectTypeOf, it } from "vitest"
+import getInitials from "~~/shared/utils/initials"
+
+describe("Test shared initials", async () => {
+    it("getInitials", () => {
+        expect(getInitials("")).toBe("")
+        expect(getInitials("John")).toBe("JO")
+        expect(getInitials("Max Musterman")).toBe("MM")
+        expect(getInitials("Max Müller Musterman")).toBe("MMM")
+    })
+})
