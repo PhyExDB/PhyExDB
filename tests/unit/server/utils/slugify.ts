5e2879d4d2ca8b03ec3f92ec7d20d946afa72873
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-02-13 17:08:11 +0100

    slugify

--- tests/unit/server/utils/slugify.ts
+++ tests/unit/server/utils/slugify.ts
@@ -1,9 +1,10 @@
-import { describe, expect, expectTypeOf, it } from "vitest"
+import { describe, expect, it } from "vitest"
 
 import slugify from "~~/server/utils/slugify"
 
 describe("Test utils slugify", async () => {
     it("slugify", () => {
-        
+        expect(slugify("Hello World")).toBe("hello-world")
+        expect(slugify("AAA ä")).toBe("aaa-a")
     })
 })
