a0a6c0ec6e06bbfde04ca590e16a0b0c32d4fb17
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-02-22 16:41:52 +0100

    Revert "auth client singleton"
    This reverts commit 433165d31d4a19284179423163e435c675ed2cd3.

--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -1,31 +1,17 @@
 import { adminClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/vue"
 
-type Client = ReturnType<typeof createAuthClient<{
-  plugins: [ReturnType<typeof adminClient>],
-  baseURL: string
-}>>
-
-let auth: {
-  client: Client,
-  // session: ReturnType<Client["useSession"]>
-}
-
 /**
  * Auth client instance configured with plugins.
  */
 export const useAuth = () => {
-  if(!auth){
-    const url = useRequestURL()
-    const client = createAuthClient({
-      plugins: [adminClient()],
-      baseURL: url.origin,
-    })
+  const url = useRequestURL()
+  const client = createAuthClient({
+    plugins: [adminClient()],
+    baseURL: url.origin,
+  })
 
-    const session = client.useSession(useFetch)
+  const session = client.useSession(useFetch)
 
-    auth = { client }
-    // return { client, session }
-  }
-  return auth
+  return { session, client }
 }
