6fbf08ab034378e6c842639c45283dde70364679
Tim-Kroker <118850777+Tim-Kroker@users.noreply.github.com>
2025-02-25 21:37:37 +0100

    Advanced search (#432)
    * advanced search
    
    * fix teamscale
    
    * fix
    
    * Also check boxes when clicking on text
    
    ---------
    
    Co-authored-by: niklhut <49069026+niklhut@users.noreply.github.com>

--- server/api/experiments/index.get.ts
+++ server/api/experiments/index.get.ts
@@ -30,15 +30,66 @@ export default defineEventHandler(async (event) => {
   }
 
   // Searching
-  const searchString = query.search as string || undefined
-  const search = searchString ? { name: { contains: searchString, mode: Prisma.QueryMode.insensitive } } : undefined
+  const querySearchString = query.search as string || undefined
+  const querySearchSections = (typeof query.sections === "string" && query.sections.length > 0)
+    ? query.sections.split(",")
+    : undefined
+  let shouldSearchTitle = false
+  let shouldSearchSections = false
+  if (querySearchSections?.includes("titel")) {
+    shouldSearchTitle = true
+    shouldSearchSections = querySearchSections.length > 1 ? true : false
+  } else {
+    shouldSearchSections = true
+  }
+
+  const searchTitle = shouldSearchTitle
+    ? {
+        name: {
+          contains: querySearchString,
+          mode: Prisma.QueryMode.insensitive,
+        },
+      }
+    : undefined
+
+  const searchSections = shouldSearchSections
+    ? {
+        sections: {
+          some: {
+            experimentSection: {
+              slug: {
+                in: shouldSearchSections ? querySearchSections : undefined,
+              },
+            },
+            text: {
+              contains: querySearchString,
+              mode: Prisma.QueryMode.insensitive,
+            },
+          },
+        },
+      }
+    : undefined
+
+  let searchCondition
+  if ((shouldSearchTitle && shouldSearchSections) || (!shouldSearchTitle && shouldSearchSections)) {
+    searchCondition = {
+      OR: [
+        { ...searchTitle },
+        { ...searchSections },
+      ],
+    }
+  } else if (shouldSearchTitle) {
+    searchCondition = { ...searchTitle }
+  } else {
+    searchCondition = undefined
+  }
 
   // Total Number of Experiments
   const totalExperiments = await prisma.experiment.count({
     where: {
       status: "PUBLISHED",
       ...attributeFilter,
-      ...search,
+      ...searchCondition,
     },
   })
 
@@ -51,7 +102,7 @@ export default defineEventHandler(async (event) => {
     where: {
       status: "PUBLISHED",
       ...attributeFilter,
-      ...search,
+      ...searchCondition,
     },
     include: experimentIncludeForToList,
     orderBy: sortOption,
