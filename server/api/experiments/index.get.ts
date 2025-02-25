f26099c3d4a0be41eb77809152b3b6e4d90313c6
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-02-25 21:46:52 +0100

    proof of concept

--- server/api/experiments/index.get.ts
+++ server/api/experiments/index.get.ts
@@ -1,5 +1,5 @@
-import { Prisma } from "@prisma/client"
-import type { Page } from "~~/shared/types"
+import { Experiment, Prisma, File } from "@prisma/client"
+import type { ExperimentIncorrectList, Page } from "~~/shared/types"
 
 export default defineEventHandler(async (event) => {
   // Attribute Filter
@@ -45,22 +45,73 @@ export default defineEventHandler(async (event) => {
   // Pagination
   const pageMeta = getPageMeta(event, totalExperiments)
 
-  // Experiment Data
-  const experiments = await prisma.experiment.findMany({
-    ...getPaginationPrismaParam(pageMeta),
-    where: {
-      status: "PUBLISHED",
-      ...attributeFilter,
-      ...search,
-    },
-    include: experimentIncludeForToList,
-    orderBy: sortOption,
-  })
+  // const result: (Experiment & File)[] = await prisma.$queryRaw`
+  // SELECT *, similarity(name, ${searchString}) FROM "public"."Experiment" as e
+
+  // LEFT JOIN "public"."File" as file ON e."previewImageId" = file."id"
+  // LEFT JOIN "public"."_ExperimentToExperimentAttributeValue" as etea ON e."id" = etea."A"
+  // LEFT JOIN "public"."ExperimentAttributeValue" as eav ON etea."B" = eav."id"
+
+  // WHERE e.status = 'PUBLISHED' 
+  
+  // GROUP BY e.id
+  
+  // ORDER BY similarity(e."name", ${searchString}) DESC
+
+  // LIMIT ${pageMeta.pageSize}
+  // OFFSET ${(pageMeta.page - 1 ) * pageMeta.pageSize}`
+
+  const result: (Experiment & File)[] = await prisma.$queryRaw`
+    SELECT *, similarity(name, ${searchString}) FROM "public"."Experiment" as e
+
+    WHERE e.status = 'PUBLISHED' and similarity(name, ${searchString}) > 0.001
+    
+    ORDER BY similarity(e."name", ${searchString}) DESC
+
+    LIMIT ${pageMeta.pageSize}
+    OFFSET ${(pageMeta.page - 1 ) * pageMeta.pageSize}`
 
   return {
-    items: experiments.map(experiment => mapExperimentToList(experiment as ExperimentIncorrectList)),
+    items: result.map(
+      experiment => mapExperimentToList(
+        { 
+          ...experiment, 
+          previewImage: undefined,
+          attributes: [], 
+          revisionOf: undefined,
+          revisedBy: undefined,
+        } as ExperimentIncorrectList
+      )
+    ),
     pagination: pageMeta,
-  } as Page<ExperimentList>
+  } // as Page<ExperimentList>
+  
+  //   const experiments = await prisma.experiment.findMany({
+  //     ...getPaginationPrismaParam(pageMeta),
+  //     where: {
+  //       id: {
+  //         in: result.map(experiment => experiment.id)
+  //       }
+  //     },
+  //     include: experimentIncludeForToList,
+  //   })
+  
+  // // // Experiment Data
+  // // const experiments = await prisma.experiment.findMany({
+  // //   ...getPaginationPrismaParam(pageMeta),
+  // //   where: {
+  // //     status: "PUBLISHED",
+  // //     ...attributeFilter,
+  // //     ...search,
+  // //   },
+  // //   include: experimentIncludeForToList,
+  // //   orderBy: sortOption,
+  // // })
+
+  // return {
+  //   items: experiments.map(experiment => mapExperimentToList(experiment as ExperimentIncorrectList)),
+  //   pagination: pageMeta,
+  // } as Page<ExperimentList>
 })
 
 defineRouteMeta({
