eda01c64ab38b077a5b6f387236f78541049afe4
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-02-25 22:11:16 +0100

    mocking Prisma to filter is not worth it

--- tests/unit/server/api/users/index.get.test.ts
+++ tests/unit/server/api/users/index.get.test.ts
@@ -26,57 +26,5 @@ describe("Api Route GET /api/users/index", () => {
     expectTypeOf<EndpointResult<typeof endpoint>>().toEqualTypeOf<typeof expected>()
 
     u.testSuccessWithPagination(context, data)
-
-    it("search by name", async () => {
-      const c = u.extendContext(context,
-        {
-          query: { search: "user" },
-          expected: u.page(
-            data
-              .filter(user => user.name.includes("user"))
-              .map(user => ({ ...user, banned: false })),
-          ),
-        },
-      )
-      u.expectSuccess(c)
-    })
-    it("search by id", async () => {
-      const c = u.extendContext(context,
-        {
-          query: { search: users.user.id },
-          expected: u.page(
-            [users.user]
-              .map(user => ({ ...user, banned: false })),
-          ),
-        },
-      )
-      u.expectSuccess(c)
-    })
-    it("search by role", async () => {
-      {
-        const c = u.extendContext(context,
-          {
-            query: { search: "moderator" },
-            expected: u.page(
-              [users.mod]
-                .map(user => ({ ...user, banned: false })),
-            ),
-          },
-        )
-        u.expectSuccess(c)
-      }
-      {
-        const c = u.extendContext(context,
-          {
-            query: { search: "admin" },
-            expected: u.page(
-              [users.admin]
-                .map(user => ({ ...user, banned: false })),
-            ),
-          },
-        )
-        u.expectSuccess(c)
-      }
-    })
   }
 })
