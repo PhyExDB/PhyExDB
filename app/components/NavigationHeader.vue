f7f767a309eb142cc891c69494aedcd8f0637f6d
niklhut <49069026+niklhut@users.noreply.github.com>
2025-02-26 18:43:03 +0100

    Hide versuche entdecken on mobile

--- app/components/NavigationHeader.vue
+++ app/components/NavigationHeader.vue
@@ -27,7 +27,7 @@
     >
       <Button
         variant="link"
-        class="text-muted-foreground"
+        class="text-muted-foreground hidden sm:block"
       >
         Versuche entdecken
       </Button>
