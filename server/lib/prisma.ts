f5a98d301752fe46eec4f5ebfb1d6957ba15255e
crafter23456 <64259198+crafter23456@users.noreply.github.com>
2025-12-21 14:18:19 +0100

    Bump dependencies and update Prisma configuration in favor of new version

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,27 +1,21 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "~~/prisma/generated/prisma/client"
 import { dbLogger } from "./loggers"
+import {PrismaPg} from "@prisma/adapter-pg";
 
 const prismaClientSingleton = () => {
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  });
+
   const prisma = new PrismaClient({
+    adapter,
     log: [
-      {
-        emit: "event",
-        level: "query",
-      },
-      {
-        emit: "event",
-        level: "error",
-      },
-      {
-        emit: "event",
-        level: "info",
-      },
-      {
-        emit: "event",
-        level: "warn",
-      },
+      { emit: "event", level: "query" },
+      { emit: "event", level: "error" },
+      { emit: "event", level: "info" },
+      { emit: "event", level: "warn" },
     ],
-  })
+  });
 
   prisma.$on("query", (e) => {
     if (process.env.NODE_ENV !== "test") {
