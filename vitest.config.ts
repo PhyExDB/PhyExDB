c16fec32c17851e68ac6548f39ae8c70184cb596
crafter23456 <64259198+crafter23456@users.noreply.github.com>
2026-02-06 21:10:34 +0100

    Add Tailwind CSS plugin to Vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
 import { defineVitestConfig } from "@nuxt/test-utils/config"
+import tailwindcss from "@tailwindcss/vite"
 import AutoImport from "unplugin-auto-import/vite"
-
 export default defineVitestConfig({
   plugins: [
+    tailwindcss(),
     AutoImport({
       imports: [
         "vue",
