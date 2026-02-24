cbd095103496573710da279743d578d24221285b
meri452 <yengoyan.meri@icloud.com>
2026-02-24 13:31:26 +0100

    modified:   server/api/experiments/submit/[id].post.ts

--- server/api/experiments/submit/[id].post.ts
+++ server/api/experiments/submit/[id].post.ts
@@ -8,7 +8,6 @@ export default defineEventHandler(async (event) => {
     where: getIdPrismaWhereClause(event),
     include: {
       ...experimentIncludeForToDetail,
-      signs: true,
     },
   })
 
